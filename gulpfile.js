f4f044b840c63127775bc1383e7e21885544aa3f
Karl Tynan <hello@karltynan.co.uk>
2020-02-21 11:59:41 +0000

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,107 @@
 /*
-	Gulp tasks for: tailwind-starter
-	Author: hello@karltynan.co.uk
+	Gulp tasks
+	Author: @karltynan
 */
 
 var gulp = require('gulp');
+var del = require('del');
+var concat = require('gulp-concat');
+var filter = require('gulp-filter');
 var postcss = require('gulp-postcss');
-var cssimport = require("gulp-cssimport");
-var cssnano = require('gulp-cssnano');
-var tailwindcss = require('tailwindcss');
-var uglifycss = require('gulp-uglifycss');
+var purgecss = require('gulp-purgecss');
 var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
 
-// css
-gulp.task('css', function () {
-	return gulp.src([
-		'./src/css/*.css'
-	])
-		.pipe(cssimport())
+var cssSourcePath = './src/css/*.css';
+var cssCopyPath = './src/css/copy**/*';
+var cssOutputPath = './css/';
+
+var jsSourcePath = [
+	'./src/js/plugins/**/*.js',
+	'./src/js/layout/**/*.js',
+	'./src/js/modules/**/*.js',
+	'./src/js/site.js'
+];
+var jsPlugins = ['./node_modules/blazy/blazy.min.js'];
+var jsCopyPath = './src/js/copy/**/*';
+var jsOutputPath = './js/';
+
+var uglifyCssOptions = {
+	"maxLineLen": 312,
+	"uglyComments": true
+};
+
+// css build
+function css_build() {
+	// delete old files
+	del([cssOutputPath + '**/*'], { force: true });
+	// create new files
+	return gulp.src(cssSourcePath)
 		.pipe(postcss([
-			tailwindcss('./tailwind.js'),
+			require('tailwindcss'),
 			require('autoprefixer')
 		]))
-		.pipe(gulp.dest('./css/'))
-		.pipe(cssnano())
-		.pipe(uglifycss({
-			'maxLineLen': 312,
-			'uglyComments': true
-		}))
+		.pipe(gulp.dest(cssOutputPath))
+		.pipe(uglifycss(uglifyCssOptions))
+		.pipe(
+			purgecss({
+				content: [
+					'./*.html',
+					'./Views/**/*.cshtml',
+					'./src/js/**/*.js'
+				],
+				extractors: [
+					{
+						extractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+						extensions: ['html', 'cshtml', 'js']
+					}
+				]
+			})
+		)
 		.pipe(rename({ suffix: '.min' }))
-		.pipe(gulp.dest('./css/'));
-});
+		.pipe(gulp.dest(cssOutputPath));
+}
+// css copy
+function css_copy() {
+	return gulp.src(cssCopyPath)
+		.pipe(gulp.dest(cssOutputPath))
+		.pipe(filter('**/*.css'))
+		.pipe(uglifycss(uglifyCssOptions))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest(cssOutputPath));
+}
+
+// js build
+function js_build () {
+	// delete old files
+	del([jsOutputPath + '**/*'], { force: true });
+	// create new files
+	return gulp.src(jsPlugins.concat(jsSourcePath))
+		.pipe(concat('site.js'))
+		.pipe(gulp.dest(jsOutputPath))
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest(jsOutputPath));
+}
+
+// js copy
+function js_copy() {
+	return gulp.src(jsCopyPath)
+		.pipe(gulp.dest(jsOutputPath))
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest(jsOutputPath));
+}
 
 // watch
-gulp.task('watch', ['build'], function () {
-	gulp.watch(['./src/css/**/*.css', './tailwind.js'], ['css']);
-});
+function watch_task() {
+	gulp.watch(['./*.html', './Views/**/*.cshtml', './src/css/**/*.css', './src/js/**/*.js', './tailwind.config.js'], css_build);
+	gulp.watch(['./src/js/**/*.js'], js_build);
+}
 
-// build
-gulp.task('build', ['css']);
+const css_task = gulp.series(css_build, css_copy);
+const js_task = gulp.series(js_build, js_copy);
+
+exports.build = gulp.parallel(css_task, js_task);
+exports.watch = gulp.series(gulp.parallel(css_task, js_task), watch_task);
